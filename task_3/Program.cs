592d1bbe73569b7945b5164075f61d7538f50d02
Vlad@ <chazovvlad17@mail.ru>
2023-10-13 21:56:50 +0300

    Сохранение дз 3

---
+++ task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число числа: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.Write("Число четное!");
+}
+else
+{
+    Console.WriteLine("Число не является четным!");
+}
