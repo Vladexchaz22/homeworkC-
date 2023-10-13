38038c65b5efb441eac9b86e12a0e2c45f07b1b3
Vlad@ <chazovvlad17@mail.ru>
2023-10-13 21:46:29 +0300

    Сохранение дз 1

---
+++ task_1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите два числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (num1 > num2)
+{
+    max = num1;
+    Console.WriteLine(num1);
+}
+else
+{
+    max = num2;
+    Console.WriteLine(num2);
+}
